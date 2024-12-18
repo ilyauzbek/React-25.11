550e02f40bf868803aeff507204140a8114fac07
ilyauzbek <Stas.yun.82@gmail.com>
2024-12-18 13:15:40 +0500

    Add files via upload

--- src/components/ReviewsWrap/ReviewsWrap.jsx
+++ src/components/ReviewsWrap/ReviewsWrap.jsx
@@ -18,10 +18,66 @@ const ReviewsWrap = () => {
                 <p>"I absolutely love this t-shirt! The design is unique and the fabric feels so comfortable. As a fellow designer, I appreciate the attention to detail. It's become my favorite go-to shirt."</p>
                 <b>Posted on August 14, 2023</b>
                 </div>
+                <div className={s.card}>
+                    <div>
+                        <p>⭐⭐⭐⭐</p>
+                        <img src="/Frame 27.svg" alt="" />
+                    </div>
+
+
+                <h3>Samantha D.✅</h3>
+                <p>"I absolutely love this t-shirt! The design is unique and the fabric feels so comfortable. As a fellow designer, I appreciate the attention to detail. It's become my favorite go-to shirt."</p>
+                <b>Posted on August 14, 2023</b>
+                </div>
+                <div className={s.card}>
+                    <div>
+                        <p>⭐⭐⭐⭐</p>
+                        <img src="/Frame 27.svg" alt="" />
+                    </div>
+
+
+                <h3>Samantha D.✅</h3>
+                <p>"I absolutely love this t-shirt! The design is unique and the fabric feels so comfortable. As a fellow designer, I appreciate the attention to detail. It's become my favorite go-to shirt."</p>
+                <b>Posted on August 14, 2023</b>
+                </div>
+                <div className={s.card}>
+                    <div>
+                        <p>⭐⭐⭐⭐</p>
+                        <img src="/Frame 27.svg" alt="" />
+                    </div>
+
+
+                <h3>Samantha D.✅</h3>
+                <p>"I absolutely love this t-shirt! The design is unique and the fabric feels so comfortable. As a fellow designer, I appreciate the attention to detail. It's become my favorite go-to shirt."</p>
+                <b>Posted on August 14, 2023</b>
+                </div>
+                <div className={s.card}>
+                    <div>
+                        <p>⭐⭐⭐⭐</p>
+                        <img src="/Frame 27.svg" alt="" />
+                    </div>
+
+
+                <h3>Samantha D.✅</h3>
+                <p>"I absolutely love this t-shirt! The design is unique and the fabric feels so comfortable. As a fellow designer, I appreciate the attention to detail. It's become my favorite go-to shirt."</p>
+                <b>Posted on August 14, 2023</b>
+                </div>
+                <div className={s.card}>
+                    <div>
+                        <p>⭐⭐⭐⭐</p>
+                        <img src="/Frame 27.svg" alt="" />
+                    </div>
+
+
+                <h3>Samantha D.✅</h3>
+                <p>"I absolutely love this t-shirt! The design is unique and the fabric feels so comfortable. As a fellow designer, I appreciate the attention to detail. It's become my favorite go-to shirt."</p>
+                <b>Posted on August 14, 2023</b>
+                </div>
             </div>
     </section>
     </>
   )
+
 }
 
 export default ReviewsWrap
